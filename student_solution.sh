d003ace074bb403ce59615c2af9af1d4a660a502
Ajaylc7 <ronaldoajay80@gmail.com>
2026-09-11 12:31:11 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -7,8 +7,8 @@
 # SELinux Permanent File Context Assignment
 # ============================================================
 #
-# Student Name:
-# Register Number:
+# Student Name: Hisham M
+# Register Number: 1U24IT044
 #
 # Complete all TODO sections.
 # Do not modify the test files.
@@ -24,19 +24,23 @@ echo "=========================================="
 # TODO 1:
 # Create the directory /webdata/files
 # ------------------------------------------------------------
+mkdir -p /webdata/files
+
 
 
 # ------------------------------------------------------------
 # TODO 2:
 # Create the file /webdata/files/index.html
 # ------------------------------------------------------------
-
+touch /webdata/files/index.html
 
 # ------------------------------------------------------------
 # TODO 3:
 # Display the current SELinux context
 # of /webdata and index.html
 # ------------------------------------------------------------
+ls -Zd /webdata
+ls -Z /webdata/files/index.html
 
 
 # ------------------------------------------------------------
@@ -49,20 +53,22 @@ echo "=========================================="
 #
 # Use semanage fcontext
 # ------------------------------------------------------------
-
+semanage fcontext -a -t httpd_sys_content_t '/webdata(/.*)?'
 
 # ------------------------------------------------------------
 # TODO 5:
 # Apply the permanent SELinux rule
 # using restorecon recursively.
 # ------------------------------------------------------------
-
+restorecon -Rv /webdata
 
 # ------------------------------------------------------------
 # TODO 6:
 # Verify the final SELinux contexts
 # of /webdata and index.html
 # ------------------------------------------------------------
+ls -Zd /webdata
+ls -Z /webdata/files/index.html
 
 
 echo "=========================================="
